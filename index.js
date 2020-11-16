dcd938a798fbedaba879f815ba3f1918f81c5d8f
Vincent Servio <vincentservio@gmail.com>
2020-11-16 15:22:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function yourAnimal(){
 }
 
 function add2(n) {
-  return n + two
+  return n + 2
 
   // Feel free to move things around!
-  const two = 2
+ 
 }
 
 var funkyFunction = function() {
